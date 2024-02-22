d6bfe373c9d159d1afd2775dd954b4e20ab31931
Nicholas Gioachini <nicholas.gioachini@arpedio.com>
2024-02-22 13:20:46 +0100

    syntax

--- Scratch.sh
+++ Scratch.sh
@@ -1,3 +1,3 @@
 
 echo "Building your preview org, please wait."
-sf org create sandbox --name mysandbox --license-type Developer --alias MyDevSandbox --target-org DevHub --set-default
+sf org create sandbox --name mysandbox --license-type Developer --alias MyDevSandbox --target-org devHub --set-default
